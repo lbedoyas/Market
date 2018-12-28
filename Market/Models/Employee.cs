cc942970ddba61e16990ead600101bf8f9beae3c
LB <lucho.bedoya1@hotmail.com>
2018-12-28 12:47:07 -0500

    Prop Age de solo lectura
    Se adiciona en el modelo Employee una propiedad de calculo de edad con el fin de saber la edad de los empleados sin agregarse a la BD

--- Market/Models/Employee.cs
+++ Market/Models/Employee.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -49,8 +50,13 @@ namespace Market.Models
         [DataType(DataType.Url)]
         public string URL { get; set; }
 
+
+
         [Required(ErrorMessage = "Ingrese por favor el {0}")]
         public int DocumentTypeID { get; set; }
+
+        [NotMapped]
+        public int Age { get { return DateTime.Now.Year - DateOfBirth.Year;}  }
 
         public virtual DocumentType DocumentType { get; set; }
 

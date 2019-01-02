53ee33cebe9d140c2984acd7cbc8c461ead4192a
LB <lucho.bedoya1@hotmail.com>
2019-01-02 15:52:09 -0500

    Adiciona en el OrderController Un listado de productos agregados
    Se adiciona dentro del ActionResult AddProduct una tabla con los productos que se van agregando que se buscan en la base de datos por medio de Linq.

--- Market/Controllers/OrdersController.cs
+++ Market/Controllers/OrdersController.cs
@@ -18,6 +18,7 @@ namespace Market.Controllers
             var orderView = new OrderView();
             orderView.Customer = new Customer();
             orderView.Products = new List<ProductOrder>();
+            Session["orderView"] = orderView;
 
             var lista = db.Customers.ToList();
             lista.Add(new Customer { CustomerId = 0, FirstName = "[Seleccion un cliente ... ]"} );
@@ -29,6 +30,8 @@ namespace Market.Controllers
         [HttpPost]
         public ActionResult NewOrder(OrderView orderView)
         {
+
+            
             var lista = db.Customers.ToList();
             lista = lista.OrderBy(c => c.FullName).ToList();
             lista.Add(new Customer { CustomerId = 0, FirstName = "[Seleccion un cliente ... ]" });
@@ -36,25 +39,76 @@ namespace Market.Controllers
             return View(orderView);
         }
 
-        public ActionResult AddProduct(ProductOrder productOrder)
+        public ActionResult AddProduct()
         {
-            var lista = db.Products.ToList();
-            lista.Add(new ProductOrder { ProductID = 0, Description = "[Seleccione un producto...] " });
-            lista = lista.OrderBy(p => p.Description).ToList();
-            ViewBag.ProductID = new SelectList(lista, "ProductID", "Description");
 
-            return View(productOrder);
-        }
-
-        [HttpPost]
-        public ActionResult AddProduct(FormCollection form)
-        {
             var lista = db.Products.ToList();
             lista.Add(new ProductOrder { ProductID = 0, Description = "[Seleccione un producto...] " });
             lista = lista.OrderBy(p => p.Description).ToList();
             ViewBag.ProductID = new SelectList(lista, "ProductID", "Description");
 
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddProduct(ProductOrder productOrder)
+        {
+            var orderView = Session["orderView"] as OrderView;
+
+            var productID = int.Parse(Request["ProductID"]);
+            if(productID == 0)
+            {
+                var lista = db.Products.ToList();
+                lista.Add(new ProductOrder { ProductID = 0, Description = "[Seleccione un producto...] " });
+                lista = lista.OrderBy(p => p.Description).ToList();
+                ViewBag.ProductID = new SelectList(lista, "ProductID", "Description");
+                ViewBag.Error = "Debe seleccionar un producto";
+                return View(productOrder);
+            }
+
+            //Este campo es para validar si el producto si existe 
+            var product = db.Products.Find(productID);
+            if (product == null)
+            {
+                var lista = db.Products.ToList();
+                lista.Add(new ProductOrder { ProductID = 0, Description = "[Seleccione un producto...] " });
+                lista = lista.OrderBy(p => p.Description).ToList();
+                ViewBag.ProductID = new SelectList(lista, "ProductID", "Description");
+                ViewBag.Error = "Producto No existe";
+                return View(productOrder);
+            }
+
+
+            productOrder = orderView.Products.Find(p => p.ProductID == productID);
+            if(productOrder == null)
+            {
+                productOrder = new ProductOrder
+                {
+                    Description = product.Description,
+                    Price = product.Price,
+                    ProductID = product.ProductID,
+                    Quantity = float.Parse(Request["Quantity"]),
+
+                };
+                orderView.Products.Add(productOrder);
+            }
+            else
+            {
+                productOrder.Quantity += float.Parse(Request["Quantity"]);
+            }
+
+            
+
+            
+
+
+            var listaC = db.Customers.ToList();
+            listaC = listaC.OrderBy(c => c.FullName).ToList();
+            listaC.Add(new Customer { CustomerId = 0, FirstName = "[Seleccion un cliente ... ]" });
+            ViewBag.CustomerId = new SelectList(listaC, "CustomerId", "FullName");
+
+
+            return View("NewOrder", orderView);
         }
 
 
--- Market/ViewModels/OrderView.cs
+++ Market/ViewModels/OrderView.cs
@@ -9,6 +9,7 @@ namespace Market.ViewModels
     public class OrderView
     {
         public Customer Customer { get; set; }
+        public ProductOrder Product { get; set; }
 
         public List<ProductOrder> Products { get; set; }
 

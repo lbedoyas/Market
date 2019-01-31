21c41d8d9fe2db8460e5a9e74e600de69439a103
LB <lucho.bedoya1@hotmail.com>
2019-01-31 08:30:37 -0500

    Se define Modelos de usuarios
    Se realiza la creacion de los modelos de users y roles y se crea un controlador vacios y se agrega la vista.

---
+++ Market/ViewModels/RoleView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Market.ViewModels
+{
+    public class RoleView
+    {
+        public string RoleID { get; set; }
+        public string Name { get; set; }
+
+        
+    }
+}
---
+++ Market/ViewModels/UserView.cs
@@ -0,0 +1,22 @@
+﻿using Market.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Market.Models
+{
+    public class UserView
+    {
+        public string UserID { get; set; }
+        public string Name { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        public RoleView Role { get; set; }
+
+        public List<RoleView> Roles { get; set; }
+    }
+}
